2f50c528f04653d0539cf02f01fadda4bd93e08c
GágányLeonidasz <akos.gagany03@gmail.com>
2024-10-08 12:46:06 +0200

    Végeztem a feladattal

--- f2/f2.js
+++ f2/f2.js
@@ -1,6 +1,9 @@
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 
 const f2 = async (lat, long, timeStr, hour) => {
-    //TODO
+    let datakrumpli;
+    await fetch(APIAdress+`latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`)
+    .then(data=>data.json()).then(data => datakrumpli=data).then(data=> console.log(data));
+     return datakrumpli.hourly.temperature_2m.at(hour)
 }
 module.exports = f2;
